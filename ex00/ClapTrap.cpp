436086cb502915828e9c1339beeedaef5b842830
Furiosso <dgimenodemanuel@gmail.com>
2025-08-22 21:09:59 +0200

    complete ex00

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,80 @@
+# include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void) : _name("Pepe"), _hit_points(10), _energy_points(10), _attack_damage(0)
+{
+	std::cout << "ClapTrap Pepe has been created" << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string name) : _name(name), _hit_points(10), _energy_points(10), _attack_damage(0)
+{
+	std::cout << "ClapTrap " << name << " has been created" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& src)
+{
+	*this = src;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "ClapTrap " << this->_name << " has been destroyed" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& rhs)
+{
+	this->_name = rhs.getName();
+	this->_attack_damage = rhs.getAttackDamage();
+	this->_energy_points = rhs.getEnergyPoints();
+	this->_hit_points = rhs.getHitPoints();
+	return *this;
+}
+
+std::string		ClapTrap::getName(void) const { return this->_name; }
+
+unsigned int	ClapTrap::getHitPoints(void) const { return this->_hit_points; }
+
+unsigned int	ClapTrap::getEnergyPoints(void) const { return this->_energy_points; }
+
+unsigned int	ClapTrap::getAttackDamage(void) const { return this->_attack_damage; }
+
+void	ClapTrap::attack(const std::string& target)
+{
+	if (this->_energy_points && this->_hit_points)
+	{
+		--(this->_energy_points);
+		std::cout << "ClapTrap " << this->_name << " attacks " << target << " causing "
+		<< this->_attack_damage << " points of damage!" << std::endl;
+		return ;
+	}
+	this->_check_points();
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	(this->_hit_points) -= amount;
+	if ((this->_hit_points) < 0)
+		(this->_hit_points = 0);
+	std::cout << "ClapTrap " << this->_name << " has taken " << amount << " points of damage resulting in a total of "
+	<< this->_hit_points << " health points!" << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_energy_points && this->_hit_points)
+	{
+		--(this->_energy_points);
+		(this->_hit_points) += amount;
+		std::cout << "ClapTrap " << this->_name << " has repaired itself whith " << amount
+		<< " health points resulting in a total amount of " << this->_hit_points << " health points" << std::endl;
+		return ;
+	}
+	this->_check_points();
+}
+
+void	ClapTrap::_check_points(void)
+{
+	if (this->_hit_points == 0)
+		std::cout << "ClapTrap " << this->_name << " is dead" << std::endl;
+	if (this->_energy_points == 0)
+		std::cout << "ClapTrap " << this->_name << " has no energy points left" << std::endl;
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,31 @@
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap	pepe;
+	ClapTrap	luis("Luis");
+
+	luis.attack("Pepe");
+	pepe.attack("Luis");
+	luis.takeDamage(20);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+	luis.beRepaired(2);
+}
